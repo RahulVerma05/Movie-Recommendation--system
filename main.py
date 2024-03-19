046d6cd2ed340302d9c21c30d3f0ab7d1dfc8431
Rahul Verma <rkverma1605@gmail.com>
2024-03-19 17:32:26 +0530

    files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-a = 5
-b = 3
-print(a+b)
-print('Hello World')
+df_1 = pd.read_csv("tmdb_5000_credits.csv")
+df_2 = pd.read_csv("tmdb_5000_movies.csv")
+
+print(df_2)
